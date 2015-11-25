713f6b3bce8efa99c1058208ebb60476c4789dcc
Shane Auckland <shane.auckland@gmail.com>
2015-11-25 10:25:16 +0000

    Reduce dependency constraints

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description 'Provides firewall access rules for Scrutinizer'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
 
-depends 'iptables-ng', '~> 2.2.0'
-depends 'sshd', '~> 1.1.1'
+depends 'iptables-ng', '~> 2.2'
+depends 'sshd', '~> 1.1'
